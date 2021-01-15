0f7785b030e183f11cf07374a9fa9a42617a9ef3
djolee96 <milan.491989@yahoo.com>
2021-01-15 13:41:20 +0100

    Updates

---
+++ precache-manifest.ade44295bdeb8fcc80568c4c929c1769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78bf6e9a4419483d8d4eebe04a40d10",
+    "url": "/bit-crud/index.html"
+  },
+  {
+    "revision": "b39e66e68d0d874f74e6",
+    "url": "/bit-crud/static/css/main.c4e3e2cd.chunk.css"
+  },
+  {
+    "revision": "ee623d1b4f63471fe988",
+    "url": "/bit-crud/static/js/2.a5256ed8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bit-crud/static/js/2.a5256ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39e66e68d0d874f74e6",
+    "url": "/bit-crud/static/js/main.8962b602.chunk.js"
+  },
+  {
+    "revision": "e4ddff8e009e37a95770",
+    "url": "/bit-crud/static/js/runtime-main.99f13def.js"
+  }
+]);
