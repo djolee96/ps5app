cb876d6c788182ee59e751a85b6c1bdee984272c
djolee96 <milan.491989@yahoo.com>
2021-01-15 13:47:02 +0100

    Updates

---
+++ precache-manifest.fd8dc5b550459d1149cecf5d36218465.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "280e57bcb61eb8aba8b2ca98fe4376f2",
+    "url": "/ps5app/index.html"
+  },
+  {
+    "revision": "e164718d58a7474a737a",
+    "url": "/ps5app/static/css/main.c4e3e2cd.chunk.css"
+  },
+  {
+    "revision": "ee623d1b4f63471fe988",
+    "url": "/ps5app/static/js/2.a5256ed8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ps5app/static/js/2.a5256ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e164718d58a7474a737a",
+    "url": "/ps5app/static/js/main.5a874b04.chunk.js"
+  },
+  {
+    "revision": "411dc1a8d374fac9d499",
+    "url": "/ps5app/static/js/runtime-main.81fbdc3d.js"
+  }
+]);
